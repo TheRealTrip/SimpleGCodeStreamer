38ffead473741a619c9e06fcd5196aa2cbba2a16
TheRealTrip <oisin.oconnor101@gmail.com>
2025-03-11 18:20:23 +0000

    Step and Go To inputs are now less prone to crashing the program, oh and step works too which helps

--- controllers/uiController.py
+++ controllers/uiController.py
@@ -16,10 +16,10 @@ class mainUI():
             self.absolute_positioning = True
 
         # Create buttons and associate them with the move function
-        btn_up = tk.Button(self.root, text="Up", command=lambda: self.connection.move("up", step))
-        btn_down = tk.Button(self.root, text="Down", command=lambda: self.connection.move("down", step) )
-        btn_left = tk.Button(self.root, text="Left", command=lambda: self.connection.move("left", step) )
-        btn_right = tk.Button(self.root, text="Right", command=lambda: self.connection.move("right", step) )
+        btn_up = tk.Button(self.root, text="Up", command=lambda: self.connection.move("up", int(self.removeNonIntegers(stepBox.get("1.0", "end-1c")))))
+        btn_down = tk.Button(self.root, text="Down", command=lambda: self.connection.move("down", int(self.removeNonIntegers(stepBox.get("1.0", "end-1c")))) )
+        btn_left = tk.Button(self.root, text="Left", command=lambda: self.connection.move("left", int(self.removeNonIntegers(stepBox.get("1.0", "end-1c")))) )
+        btn_right = tk.Button(self.root, text="Right", command=lambda: self.connection.move("right", int(self.removeNonIntegers(stepBox.get("1.0", "end-1c")))) )
 
 
         #Getting the step distance for relative motion
@@ -32,7 +32,7 @@ class mainUI():
         yCoordBox = tk.Text(self.root, width=3, height=1)
         xCoordLabel = tk.Label(self.root, text="X-Coord")
         yCoordLabel = tk.Label(self.root, text="Y-Coord")
-        goToButton = tk.Button(self.root, text="Go To", command=lambda: self.connection.goTo(int(xCoordBox.get("1.0", "end-1c")), y = int(yCoordBox.get("1.0", "end-1c"))))
+        goToButton = tk.Button(self.root, text="Go To", command=lambda: self.connection.goTo(int(self.removeNonIntegers(xCoordBox.get("1.0", "end-1c"))), y = int(self.removeNonIntegers(yCoordBox.get("1.0", "end-1c")))))
 
         #Home Button
         homeButton = tk.Button(self.root, text="Home", command=lambda: self.connection.goHome())
@@ -61,7 +61,7 @@ class mainUI():
         goToButton.grid(row=7, column=2, pady=10)
 
 
-        step = int(stepBox.get("1.0", "end-1c"))
+
 
         for i, preset in enumerate(presets): # Adding all the presets to the UI
             button = tk.Button(self.root, text=preset.name, command=lambda p=preset: p.goToPreset(connection))
@@ -85,3 +85,6 @@ class mainUI():
             self.setPositioning('relative')
         else:
             self.setPositioning('absolute')
+    def removeNonIntegers(self, input_string):
+        result = ''.join([char for char in input_string if char.isdigit()])
+        return result
